1020de83cf593feaf242b7e82497363b4151cbd4
Linus Johansson <golfarenlj@gmail.com>
2024-12-12 22:39:53 +0000

    Add null checks to prevent unnecessary console errors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,20 +2,29 @@
 async function loadDays() {
   const response = await fetch('partials/days.html');
   const data = await response.text();
-  document.getElementById('calendar-days').innerHTML = data;
+  const calendarDaysElement = document.getElementById('calendar-days');
+  if (calendarDaysElement) {
+    calendarDaysElement.innerHTML = data;
+  }
 }
 
 // Function to load the modals content
 async function loadModals() {
   const response = await fetch('partials/modals.html');
   const data = await response.text();
-  document.getElementById('modals-container').innerHTML = data;
+  const modalsContainerElement = document.getElementById('modals-container');
+  if (modalsContainerElement) {
+    modalsContainerElement.innerHTML = data;
+  }
 
   // Load modal contents for each day
   for (let i = 1; i <= 25; i++) {
     const dayResponse = await fetch(`partials/day-content/day${i}.html`);
     const content = await dayResponse.text();
-    document.getElementById(`day${i}Content`).innerHTML = content;
+    const dayContentElement = document.getElementById(`day${i}Content`);
+    if (dayContentElement) {
+      dayContentElement.innerHTML = content;
+    }
   }
 }
 
